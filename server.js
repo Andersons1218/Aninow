f06bbe76fb8a2d325357343270f9a933adc456ec
andersons1218 <andersons1218@gmail.com>
2022-06-13 11:01:11 -0400

    refactored Set up server.js

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 /////////////////////////////////////////////
 // Import Our Dependencies
 /////////////////////////////////////////////
-require("dotenv").config()
-const express = require('express')
-const morgan = require('morgan')
-const methodOverride = require('method-override')
-const UserRouter = require("./controller/users")
-//const path = require('path')
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const path = require("path")
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+// Setup inputs for our connect function
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Establish Connection
+mongoose.connect(DATABASE_URL, CONFIG);
+
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const { Schema, model } = mongoose;
+
+// make anime schema
+const animeSchema = new Schema({
+  name: String,
+  image: String,
+  info: String,
+});
+
+// make Anime model
+const Anime = model("Anime", animeSchema);
 
 /////////////////////////////////////////////////
-// Create our Express Application Object Bind Liquid Templating Engine
+// Create our Express Application Object
 /////////////////////////////////////////////////
-const app = require('liquid-express-views')(express()) //{root: [path.resolve(__dirname, 'views/')]})
-
+const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
 
 /////////////////////////////////////////////////////
 // Middleware
@@ -25,16 +59,17 @@ app.use(express.static("public")); // serve files from public statically
 ////////////////////////////////////////////
 // Routes
 ////////////////////////////////////////////
-
-app.use('/users', UserRouter)
-
 app.get("/", (req, res) => {
-    res.render("index.liquid");
-  })
+  res.send("your server is running... better catch it.");
+});
+
+
+
+
 
 
 //////////////////////////////////////////////
 // Server Listener
 //////////////////////////////////////////////
-const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
